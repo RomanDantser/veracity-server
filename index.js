a03b634d00fb9c98f270d176c1085546126f3250
Roman Dantser <romandantser@gmail.com>
2023-06-12 11:15:35 +0700

    Added Dockerfiles for future support + added prod env var for depoloying on VDS

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json( {limit: '50mb'}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: process.env.ORIGIN_DEV,
     credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
